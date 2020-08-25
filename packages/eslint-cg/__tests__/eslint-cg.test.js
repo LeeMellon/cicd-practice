2c820855458c92cd682db7fea507db20d01cb7d8
IMGoodrich <igoodrich@phase2technology.com>
2020-08-25 13:51:30 -0700

    refactor(packages): removes packages/esling-cg

--- packages/eslint-cg/__tests__/eslint-cg.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const eslintCg = require('../lib/index');
-
-test('eslint-config', () => {
-  expect(eslintCg.plugins).toEqual(['prettier', 'eslint-plugin-import', 'react']);
-});
--- packages/eslint-cg/lib/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = eslintCg;
-
-function eslintCg() {
-    // TODO
-}
